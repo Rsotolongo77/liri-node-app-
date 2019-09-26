c263abd1e821580f5a6210a64e9bb16bb128a6a7
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-09-26 17:37:43 -0400

    added axios funtion for movies and bands in town.

--- liri.js
+++ liri.js
@@ -26,13 +26,56 @@ function song(input) {
 }
 
 function movies() {
+    axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            console.log("The movie's rating is: " + response.data.imdbRating);
+        })
+        .catch(function (error) {
+            if (error.response) {
+
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+
+                console.log(error.request);
+            } else {
+
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 
 
 }
 
 
 function concert() {
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            console.log("The movie's rating is: " + response.data.imdbRating);
+        })
+        .catch(function (error) {
+            if (error.response) {
 
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+
+                console.log(error.request);
+            } else {
+
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 
 }
 
